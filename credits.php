06dfe480ece937bffd858b013518920c48bd8fd6
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-07-05 22:05:26 -0600

    Update credits

--- credits.php
+++ credits.php
@@ -38,25 +38,44 @@ includeHeader();
 				<li><p><strong>Leonardo Julca</strong> [Slayer95] <small>– Development</small></p></li>
 			</ul>
 
-			<h2><span>Contributors</span></h2>
+			<h2><span>Major Contributors</span></h2>
 
 			<ul>
-				<li><p><strong>Andrew Goodsell</strong> [Zracknel] <small>– Art (battle weather backdrops)</small></p></li>
+				<li><p>[<strong>Anubis, Lusamine</strong>] <small>– Research (game mechanics), Sysadmin</small></p></li>
 				<li><p><strong>Austin Couturier</strong> <small>– Development (damage calculator)</small></p></li>
-				<li><p><strong>Ben Davies</strong> [Morfent] <small>– Development</small></p></li>
-				<li><p><strong>Ben Frengley</strong> [TalkTakesTime] <small>– Development</small></p></li>
-				<li><p><a href="http://sailorwanda.deviantart.com/" target="_blank" class="subtle"><strong>Catherine D.</strong> [SailorCosmos, Matryoshkat]</a> <small>– Art (battle animations)</small></p></li>
-				<li><p>[<strong>Honko</strong>] <small>– Development (damage calculator)</small></p></li>
-				<li><p><a href="https://panpawn.github.io/" target="_blank" class="subtle"><strong>Jeremy Piemonte</strong> [panpawn]</a> <small>– Development</small></p></li>
 				<li><p><strong>Kevin Lau</strong> [Ascriptmaster] <small>– Development, Art (battle animations)</small></p></li>
 				<li><p><strong>Konrad Borowski</strong> [xfix] <small>– Development</small></p></li>
 				<li><p><strong>Kris Johnson</strong> <small>– Development</small></p></li>
-				<li><p><strong>Quinton Lee</strong> [sirDonovan] <small>– Development</small></p></li>
+				<li><p><strong>Leonard Craft III</strong> [DaWoblefet] <small>– Research (game mechanics)</small></p></li>
 				<li><p><strong>Neil Rashbrook</strong> [urkerab] <small>– Development</small></p></li>
-				<li><p><strong>Robin Vandenbrande</strong> [Quinella] <small>– Development</small></p></li>
+				<li><p>[<strong>peach</strong>] <small>– Development</small></p></li>
+				<li><p><strong>Quinton Lee</strong> [sirDonovan] <small>– Development</small></p></li>
 				<li><p>[<strong>Ridaz</strong>] <small>– Art (battle animations)</small></p></li>
+				<li><p><strong>Robin Vandenbrande</strong> [Quinella] <small>– Development</small></p></li>
 				<li><p><strong>Tobias Mann</strong> [asgdf] <small>– Development</small></p></li>
-				<li><p><a href="https://github.com/Zarel/Pokemon-Showdown/blob/master/README.md#credits" target="_blank">(and the rest of our GitHub contributors)</a></p></li>
+				<li><p><strong>William</strong> [MacChaeger] <small>– Development</small></p></li>
+			</ul>
+
+			<h2><span>Contributors</span></h2>
+
+			<ul>
+				<li><p><strong>Alexander B.</strong> [mathfreak231] <small>– Development</small></p></li>
+				<li><p><strong>Andrew Goodsell</strong> [Zracknel] <small>– Art (battle weather backdrops)</small></p></li>
+				<li><p><strong>Avery Zimmer</strong> [Lyren, SolarisFox] <small>– Development</small></p></li>
+				<li><p><strong>Ben Davies</strong> [Morfent] <small>– Development</small></p></li>
+				<li><p><strong>Ben Frengley</strong> [TalkTakesTime] <small>– Development</small></p></li>
+				<li><p><a href="http://sailorwanda.deviantart.com/" target="_blank" class="subtle"><strong>Catherine D.</strong> [SailorCosmos, Matryoshkat]</a> <small>– Art (battle animations)</small></p></li>
+				<li><p><strong>Cody Thompson</strong> [Rising_Dusk] <small>– Development</small></p></li>
+				<li><p>[<strong>Honko</strong>] <small>– Development (damage calculator)</small></p></li>
+				<li><p><strong>Ian Clail</strong> [Layell] <small>– Art (battle graphics, sprites)</small></p></li>
+				<li><p><strong>Jacob McLemore</strong> <small>– Development</small></p></li>
+				<li><p><strong>Erik Bruce</strong> [Kalalokki] <small>– Art (minisprite resizing)</small></p></li>
+				<li><p><a href="https://panpawn.github.io/" target="_blank" class="subtle"><strong>Jeremy Piemonte</strong> [panpawn]</a> <small>– Development</small></p></li>
+				<li><p><a href="http://leparagon.deviantart.com/" target="_blank" class="subtle">[<strong>leparagon</strong>]</a> <small>– Art (sprites, minisprite resizing)</small></p></li>
+				<li><p><strong>Luke Harmon-Vellotti</strong> [moo, CheeseMuffin] <small>– Development</small></p></li>
+				<li><p><strong>Russell Jones</strong> [SadisticMystic] <small>– Research (game mechanics)</small></p></li>
+				<li><p><strong>Spandan Punwatkar</strong> <small>– Development</small></p></li>
+				<li><p><strong>Waleed Hassan</strong> [jetou] <small>– Development</small></p></li>
 			</ul>
 
 			<h2><span>Sponsor</span></h2>
@@ -74,8 +93,9 @@ includeHeader();
 				<li><p><a href="http://vtas.deviantart.com/" target="_blank" class="subtle"><strong>Vivian Zou</strong> [Vtas]</a> <small>– Art (Horizon background)</small></p></li>
 				<li><p><a href="http://yilx.deviantart.com/" target="_blank" class="subtle"><strong>Samuel Teo</strong> [Yilx]</a> <small>– Art (Waterfall background)</small></p></li>
 				<li><p><a href="http://quanyails.deviantart.com/" target="_blank" class="subtle"><strong>Yijing Chen</strong> [Quanyails]</a> <small>– Art (Ocean background)</small></p></li>
-				<li><p><a href="http://smogon.com/forums/threads/xy-sprite-project-read-1st-post-the-face-of-the-metagame.3486712/" target="_blank" class="subtle"><strong>X/Y</strong></a> and <a href="http://www.smogon.com/forums/threads/sun-moon-sprite-project-dawn-of-a-new-day.3577711/" target="_blank" class="subtle"><strong>Sun/Moon Sprite Projects</strong></a> led by <strong>Ian Clail</strong> [Layell]<small>– Sprites</small></p></li>
-				<li><p><a href="http://www.pkparaiso.com/xy/sprites_pokemon.php" target="_blank" class="subtle"><strong>PKPara&iacute;so</strong></a> <small>– Gen 6 and 7 sprite ripping</small></p></li>
+				<li><p><a href="http://smogon.com/forums/threads/3486712/" target="_blank" class="subtle"><strong>X/Y</strong></a> and <a href="http://www.smogon.com/forums/threads/3577711/" target="_blank" class="subtle"><strong>Sun/Moon Sprite Projects</strong></a> led by <strong>Ian Clail</strong> [Layell]<small>– Sprites</small></p></li>
+				<li><p><a href="http://www.smogon.com/forums/threads/3577711/" target="_blank" class="subtle"><strong>Sun/Moon</strong></a> and <a href="https://www.smogon.com/forums/threads/3647722/" target="_blank" class="subtle"><strong>Sword/Shield Sprite Projects</strong></a> led by [<strong>leparagon</strong>] <small>– Sprites</small></p></li>
+				<li><p><a href="http://www.pkparaiso.com/xy/sprites_pokemon.php" target="_blank" class="subtle"><strong>PKPara&iacute;so</strong></a> <small>– Gen 6, 7, and 8 sprite ripping</small></p></li>
 				<li><p><a href="http://veekun.com/" target="_blank" class="subtle"><strong>Veekun</strong></a> led by <strong>Alex Munroe</strong> [eevee] <small>– Gen 5 data</small></p></li>
 			</ul>
 
